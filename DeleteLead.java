0e8cdaee17514b45d19a3bc326240c6cc234d146
cynthiaeudes08 <123589375+cynthiaeudes08@users.noreply.github.com>
2023-02-15 10:57:16 +0530

    Add files via upload

---
+++ DeleteLead.java
@@ -0,0 +1,22 @@
+package assignments1.Week2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DeleteLead {
+
+	public static void main(String[] args) {
+		EdgeDriver driver=new EdgeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		String text=driver.findElement(By.tagName("h2")).getText();
+		System.out.println(text);
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.xpath("(//input[contains(@name,'firstName')])[3]").sendKeys("Almaz");
+
+	}
+
+}
